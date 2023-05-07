8115dede4ec4d67e7f21f225ed6ffab94182cac8
dinarfathrahmanov <dinfat@mail.ru>
2023-05-07 16:24:29 +0300

    готово

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
 
+    public static void task1() {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
 
         for (Integer num : nums) {
@@ -15,24 +21,31 @@ public class Main {
             }
 
         }
-        System.out.println();
+    }
 
-        List<Integer> nums1 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        Set<Integer> set = new TreeSet<>(nums1);
+
+    public static void task2() {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Set<Integer> set = new TreeSet<>();
         for (Integer num : nums) {
             if (num % 2 == 0) {
-                System.out.println(num);
+                set.add(num);
+            }
+        }
+                System.out.println(set);
 
 
             }
-        }
-        System.out.println();
 
+
+    public static void task3() {
         List<String> str = new ArrayList<>(List.of("qwe", "qwe", "rty", "qwe", "fgh", "sdf"));
         Set<String> st = new HashSet<>(str);
         System.out.println(st);
-        System.out.println();
+    }
 
+
+    public static void task4() {
         List<String> strings = new ArrayList<>(List.of("один", "два", "два", "два", "три", "три", "три"));
         Map<String, Integer> map = new HashMap<>();
         for (String string : strings) {
